6089a45b4036e7e63c07c4ad85dc9c7daabb6f0f
Israfil Bhagwandass <isrib135@gmail.com>
2023-03-17 12:38:39 -0400

    Added bookmark button

--- contentScript.js
+++ contentScript.js
@@ -18,12 +18,19 @@
         console.log(bookmarkBtnExists);
 
         if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
+            const bookmarkBtn = document.createElement("button");
+            bookmarkBtn.style.display = "flex"
+            bookmarkBtn.style.justifyContent = "center"
+            bookmarkBtn.style.alignItems = "center"
 
-            // Adding bookmark button
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
             bookmarkBtn.className = "ytp-button " + "bookmark-btn";
             bookmarkBtn.title = "Click to bookmark current timestamp";
+            
+            const icon = document.createElement("img")
+            icon.src = chrome.runtime.getURL("assets/bookmark.png")
+            icon.style.height = "36px"
+            icon.style.width = "36px"
+            bookmarkBtn.append(icon)
 
             youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
             youtubePlayer = document.getElementsByClassName("video-stream")[0];
@@ -34,16 +41,18 @@
     }
 
     const addNewBookmarkEventHandler = () => {
-        const currentTime = youtubePlayer.currentTime;
-        const newBookmark = {
-            time: currentTime,
-            desc: "Bookmark at " + getTime(currentTime),
-        };
-        console.log(newBookmark);
+        // const currentTime = youtubePlayer.currentTime;
+        // const newBookmark = {
+        //     time: currentTime,
+        //     desc: "Bookmark at " + getTime(currentTime),
+        // };
+        // console.log(newBookmark);
 
-        chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
-        });
+        // chrome.storage.sync.set({
+        //     [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+        // });
+
+        console.log('bookmark btn pressed')
     }
 
     newVideoLoaded();
